99d99be2363a569ca3f34e6ef4b1f7b42f884d3a
emmanuel2011 <okoyee61@gmail.com>
2025-02-20 22:10:34 +0100

    added items

--- src/js/product.js
+++ src/js/product.js
@@ -4,7 +4,7 @@ import ProductDetails from "./ProductDetails.mjs";
 
 loadHeaderFooter();
 
-const dataSource = new ProductData("laptops");
+const dataSource = new ProductData("tents");
 const productId = getParams("product");
 
 const product = new ProductDetails(productId, dataSource);
